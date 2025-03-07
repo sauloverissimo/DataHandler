16f48e4efb545f8155ff476c3af4be2f9e90aff3
sauloverissimo <sauloverissimo@gmail.com>
2025-03-07 03:02:28 -0300

    Update datahandler.cpp

---
+++ src/datahandler.cpp
@@ -0,0 +1,9 @@
+#include "datahandler.h"
+
+
+// Implementação das funções da biblioteca (se necessário)
+
+// Inicializador (se necessário)
+void setupDataHandler() {
+    Serial.println("🔹 DataHandler Library Initialized");
+}
